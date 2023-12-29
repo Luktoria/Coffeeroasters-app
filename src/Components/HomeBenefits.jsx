e6859fe8b0371b892a603a3b6571c7372a627c24
Luktoria <110913006+Luktoria@users.noreply.github.com>
2023-12-29 17:33:50 +0100

    Started adjusting tablet layouts

--- src/Components/HomeBenefits.jsx
+++ src/Components/HomeBenefits.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Benefits = () => {
   return (
-    <section>
+    <section className="benefits-section">
       <div className="benefits benefits-background">
         <h2>Why choose us?</h2>
         <p>A large part of our role is choosing which particular coffees will be featured
@@ -13,21 +13,27 @@ const Benefits = () => {
       <div className="benefits-reasons">
         <div className="benefits-item">
           <img src="./assets/home/desktop/icon-coffee-bean.svg" alt="Coffee bean icon" />
+          <div>
           <h4>Best quality</h4>
           <p>Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+          </div>
         </div>
 
         <div className="benefits-item">
           <img src="./assets/home/desktop/icon-gift.svg" alt="Gift icon" />
+          <div>
           <h4>Exclusive benefits</h4>
           <p>Special offers and swag when you subscribe, including 30% off your first shipment.</p>
+          </div>
         </div>
 
         <div className="benefits-item">
-          <img src="./assets/home/desktop/icon-truck.svg" alt="Truck icon" className="truck-icon"/>
+          <img src="./assets/home/desktop/icon-truck.svg" alt="Truck icon" id="truck-icon"/>
+          <div>
           <h4>Free shipping</h4>
-          <p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
-        </div>
+          <p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p> 
+          </div>
+          </div>
       </div>
 
     </section>
--- src/Components/HomePageCoffies.jsx
+++ src/Components/HomePageCoffies.jsx
@@ -12,26 +12,34 @@ const Collection = () => {
       <div className="collection-coffees">
         <div className="collection-item">
           <img src="./assets/home/desktop/image-gran-espresso.png" alt="coffee gran espresso" />
+          <div>
           <h4>Gran Espresso</h4>
           <p>Light and flavorful blend with cocoa and black pepper for an intense experience.</p>
+          </div>
         </div>
 
         <div className="collection-item">
         <img src="./assets/home/desktop/image-planalto.png" alt="coffee planalto" />
+        <div>
         <h4>Planalto</h4>
         <p>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.</p>
+        </div>
         </div>
 
         <div className="collection-item">
         <img src="./assets/home/desktop/image-piccollo.png" alt="coffee piccollo" />
+        <div>
         <h4>Piccollo</h4>
         <p>Mild and smooth blend featuring notes of toasted almond and dried cherry.</p>
+        </div>
         </div>
 
         <div className="collection-item">
         <img src="./assets/home/desktop/image-danche.png" alt="coffee danche" />
+        <div>
         <h4>Danche</h4>
         <p>Ethiopian hand-harvested blend densely packed with vibrant fruit notes.</p>
+        </div>
         </div>
 
       </div>
